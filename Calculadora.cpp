852bb02a41f0f49790fb90c9520bb1854f2adc9b
Maria Francisca <141454612+marifr73@users.noreply.github.com>
2024-03-05 13:01:33 -0300

    Update Calculadora.cpp

--- Calculadora.cpp
+++ Calculadora.cpp
@@ -4,44 +4,44 @@
 int main (){
 	setlocale(LC_ALL,"Portuguese");
 int x, y, o;
-printf("Informe um n˙mero inteiro: ");
+printf("Informe um n√∫mero inteiro: ");
 scanf("%i",&x);
-printf("Informe outro n˙mero inteiro: ");
+printf("Informe outro n√∫mero inteiro: ");
 scanf("%i",&y);
 
-printf("\n1 - ADI«√O\n");
-printf("2 - SUBTRA«√O\n");
-printf("3 - MULTIPLICA«√O\n");
-printf("4 - DIVIS√O\n\n");
-printf("Informe a operaÁ„o: ");
+printf("\n1 - ADI√á√ÉO\n");
+printf("2 - SUBTRA√á√ÉO\n");
+printf("3 - MULTIPLICA√á√ÉO\n");
+printf("4 - DIVIS√ÉO\n\n");
+printf("Informe a opera√ß√£o: ");
 scanf("%i", &o);  
 printf("\n================================\n");
 
 switch (o){
      
 case 1:
-	printf("A adiÁ„o de %i mais %i È: %i\n", x, y, (x + y));
+	printf("A adi√ß√£o de %i mais %i √©: %i\n", x, y, (x + y));
 	     break;
      
 case 2:
-	printf("A subtraÁ„o de %i menos %i È: %i\n", x, y, (x - y));
+	printf("A subtra√ß√£o de %i menos %i √©: %i\n", x, y, (x - y));
 	     break;
      
 case 3:
-	printf("A multiplicaÁ„o de %i por %i È de: %i\n", x, y, (x * y));
+	printf("A multiplica√ß√£o de %i por %i √© de: %i\n", x, y, (x * y));
 	     break;
      
 case 4:
 	if(y != 0) {
-				printf("A divis„o de %i por %i È de: %i\n", x, y, (x / y));
+	             printf("A divis√£o de %i por %i √© de: %i\n", x, y, (x / y));
 			} 
 else {
-	printf("Erro: Divis„o por zero n„o È possivel!\n");
+	printf("Erro: Divis√£o por zero n√£o √© possivel!\n");
 }
  break;
 
 default: 
-    printf("N„o foi encontrado o cÛdigo informado!\n");
+    printf("N√£o foi encontrado o c√≥digo informado!\n");
          break;
 }
 return(0);

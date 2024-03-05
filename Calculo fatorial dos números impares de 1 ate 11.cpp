618e3ac63b06d5c565f267ff3ac1809977231613
Maria Francisca <141454612+marifr73@users.noreply.github.com>
2024-03-05 16:35:03 -0300

    Update and rename Lista de C 3 Questão 7.cpp to Calculo fatorial dos números impares de 1 ate 11.cpp

--- Calculo fatorial dos números impares de 1 ate 11.cpp
+++ Calculo fatorial dos números impares de 1 ate 11.cpp
@@ -4,7 +4,7 @@
 int main(){
 	setlocale(LC_ALL, "Portuguese");
 int i, f = 1, cont;
-    printf("O calculo fatorial dos números impares de 1 ate 11 é de: \n");
+    printf("O calculo fatorial dos nÃºmeros impares de 1 ate 11 Ã© de: \n");
      for(i = 1; i <= 11; i+=2) {
 	f = 1;
 	    for(cont = 1; cont <= i ; cont++){

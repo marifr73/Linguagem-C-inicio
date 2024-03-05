dcbf0a24c458a6b065250d88452591d5412a4d6e
Maria Francisca <141454612+marifr73@users.noreply.github.com>
2024-03-05 15:09:38 -0300

    Update Área de uma circunferência.cpp

--- Área de uma circunferência.cpp
+++ Área de uma circunferência.cpp
@@ -6,10 +6,10 @@
 int main(){
     setlocale(LC_ALL, "Portuguese");
 float r, a;
-printf("Informe o raio da circunferência: ");
+printf("Informe o raio da circunferÃªncia: ");
 scanf("%f", &r);
 a = PI * (r * r);
-printf ("A área da circunferência é de: %.2f", a);
+printf ("A Ã¡rea da circunferÃªncia Ã© de: %.2f", a);
 return(0);
 }
 

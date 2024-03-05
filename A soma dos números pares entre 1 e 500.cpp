99baf9dedca4967d4378c4b362ff34791b844d0b
Maria Francisca <141454612+marifr73@users.noreply.github.com>
2024-03-05 16:29:39 -0300

    Update and rename Lista de C 3 Questão 2.cpp to A soma dos números pares entre 1 e 500.cpp

--- A soma dos números pares entre 1 e 500.cpp
+++ A soma dos números pares entre 1 e 500.cpp
@@ -4,12 +4,12 @@
 int main(){
 	setlocale(LC_ALL, "Portuguese");
 int cont, i = 0;
-        printf("Os números pares entre 1 e 500 são:\n");
+        printf("Os nÃºmeros pares entre 1 e 500 sÃ£o:\n");
         for(cont = 1; cont <= 500; cont++) {
             if(cont % 2 == 0) {
                i += cont;
             }
         }
-    printf("A soma dos números pares entre 1 e 500 é: %i\n", i);
+    printf("A soma dos nÃºmeros pares entre 1 e 500 Ã©: %i\n", i);
     return (0);
 }

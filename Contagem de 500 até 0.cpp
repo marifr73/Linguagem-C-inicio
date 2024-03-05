15da6f47dde7d9935ed6cf312b5ec5eddff5acef
Maria Francisca <141454612+marifr73@users.noreply.github.com>
2024-03-05 16:28:55 -0300

    Update and rename Lista de C 3 Questão 1.cpp to Contagem de 500 até 0.cpp

--- Contagem de 500 até 0.cpp
+++ Contagem de 500 até 0.cpp
@@ -4,7 +4,7 @@
 int main(){
 	setlocale(LC_ALL, "Portuguese");
 int cont;
-    printf("Contagem de 500 até 0:\n");
+    printf("Contagem de 500 atÃ© 0:\n");
     for(cont = 500; cont > 0; cont--){
     	printf("%i  ", cont);
 }

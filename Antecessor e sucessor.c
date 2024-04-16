2fcf60c2e8933a8c68fb8f519e487d8cb519807b
Maria Francisca <mariafranciscaflor73@hotmail.com>
2024-04-16 09:36:55 -0300

    Update and rename Antecessor e sucessor.cpp to Antecessor e sucessor.c

--- Antecessor e sucessor.c
+++ Antecessor e sucessor.c
@@ -4,10 +4,10 @@
 int main(){
 	setlocale(LC_ALL, "Portuguese");
     int n, a, s;
-printf("Informe um número inteiro: ");
+printf("Informe um nÃºmero inteiro: ");
 scanf("%i", &n);
 a = n - 1;
 s = n + 1;
-printf("%i é o antecessor de %i e o sucessor é %i " ,a, n, s);
+printf("%i Ã© o antecessor de %i e o sucessor Ã© %i " ,a, n, s);
 return(0);
 }

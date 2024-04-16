06a81464d0ea3a108e925e8db6fd977c5c1d9d58
Maria Francisca <mariafranciscaflor73@hotmail.com>
2024-04-16 09:40:38 -0300

    Update Maças Compradas em Duzia.cpp

--- Maças Compradas em Duzia.cpp
+++ Maças Compradas em Duzia.cpp
@@ -4,14 +4,14 @@
 int main(){
 	setlocale(LC_ALL, "Portuguese");
 float m;
-printf("Informe a quantidade da maças compradas: ");
+printf("Informe a quantidade da maÃ§as compradas: ");
 scanf("%f", &m);
 
 if (m < 12){
-	printf("O valor a ser pago pelas maças é de: %.2f", m * 1.30);
+	printf("O valor a ser pago pelas maÃ§as Ã© de: %.2f", m * 1.30);
 }
 else {
-	printf("O valor a ser pago pelas maças é de: %.2f", m * 1.10);
+	printf("O valor a ser pago pelas maÃ§as Ã© de: %.2f", m * 1.10);
 }
 return(0);
 }

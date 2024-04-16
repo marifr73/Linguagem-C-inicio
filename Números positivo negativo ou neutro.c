c24c069094c243d3efc2f1e31dcd4858bbd8d545
Maria Francisca <mariafranciscaflor73@hotmail.com>
2024-04-16 09:41:07 -0300

    Update and rename Números positivo negativo ou neutro.cpp to Números positivo negativo ou neutro.c

--- Números positivo negativo ou neutro.c
+++ Números positivo negativo ou neutro.c
@@ -4,17 +4,17 @@
 int main(){
 	setlocale(LC_ALL, "Portuguese");
 int n;
-printf("Informe um número: ");
+printf("Informe um nÃºmero: ");
 scanf("%i", &n);
 
 if (n > 0){
-	printf("%i é positivo!", n);
+	printf("%i Ã© positivo!", n);
 }
 else if(n < 0){
-	printf("%i é negativo!", n);
+	printf("%i Ã© negativo!", n);
 }
 else{
-	printf("0 é neutro!", n);
+	printf("0 Ã© neutro!", n);
 }
 return(0);
 }

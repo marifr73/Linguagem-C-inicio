ed5638df9a445db76bb334dcdb239abba3c625d2
Maria Francisca <mariafranciscaflor73@hotmail.com>
2024-04-16 09:37:25 -0300

    Update and rename CALCULAR NOTA COM MÉDIA 7.cpp to CALCULAR NOTA COM MÉDIA 7.c

--- CALCULAR NOTA COM MÉDIA 7.c
+++ CALCULAR NOTA COM MÉDIA 7.c
@@ -18,28 +18,28 @@ scanf("%f", &n3);
 mp = (n1 + n2 + n3)/3;
 
 if((mp >= 3.5) && (mp < 7)){
-	printf("A sua Média Parcia é de %.1f portanto você fara a Avaliação Final!\n\n", mp);
+	printf("A sua MÃ©dia Parcia Ã© de %.1f portanto vocÃª fara a AvaliaÃ§Ã£o Final!\n\n", mp);
 
-printf("Informe a nota da avaliação final: ");
+printf("Informe a nota da avaliaÃ§Ã£o final: ");
 scanf("%f", &af);
 
     mf = ((6 * mp) + (4 * af))/10;
 
 if (mf >= 5){
-	printf("A sua média final é %.1f portanto vecê está aprovado(a)!\n",mf);
+	printf("A sua mÃ©dia final Ã© %.1f portanto vecÃª estÃ¡ aprovado(a)!\n",mf);
 }
 
 else{
-	printf("A sua média final é %.1f portanto vecê está reprovado(a)!\n",mf);
+	printf("A sua mÃ©dia final Ã© %.1f portanto vecÃª estÃ¡ reprovado(a)!\n",mf);
 }
 
 }
 else if(mp < 3.5){
-	printf("A sua Média Parcia é de %.1f portanto você está reprovado!\n", mp);
+	printf("A sua MÃ©dia Parcia Ã© de %.1f portanto vocÃª estÃ¡ reprovado!\n", mp);
 }
 
 else{
-	printf("A sua Média Parcia é de %.1f portanto você está aprovado!\n", mp);
+	printf("A sua MÃ©dia Parcia Ã© de %.1f portanto vocÃª estÃ¡ aprovado!\n", mp);
 }
 
 return(0);
